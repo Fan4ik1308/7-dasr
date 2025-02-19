eef365b37d8a9ca75553e74e0f26bd5cdf6effa4
Fan4ik1308 <moonaraves@gmail.com>
2025-02-19 22:51:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+// for(let index = 10; index >= 0; index--){
+//     console.log(index);
+// }
+
+// for(let index = 0; index < cars.length; index++){
+//     console,log(cars[index]);
+// }
+
+// const numbers = [43,89,54,24,97,100,43,22,16,101,67];
+
+
+
+// const evenNumbers = numbers.filter(num => num % 2 === 0);
+// const oddNumbers = numbers.filter(num => num % 2 !== 0);
+
+// console.log("Четные числа:", evenNumbers);
+// console.log("Нечетные числа:", oddNumbers);
+// const numbers = [43, 89, 54, 24, 97, 100, 43, 22, 16, 101, 67];
+// for (let index = 0; index < numbers.length; index++){
+//     if(numbers[index] % 2 == 0){
+//         console.log(`juft son : ${numbers[index]}`);  
+//     }else{
+//         console.log(`toq son : ${numbers[index]}`);
+//     }
+// }
+
+
+
+// numbers = [2, 4, 9, 10, 45, 67, 8, 90]
+
+
+// even_numbers = [num for num in numbers if num % 2 == 0]
+
+
+// print("Начальный массив:", numbers)
+// print("Массив с четными числами:", even_numbers)
+
+let numbers = [2, 4, 9, 10, 45, 67, 8, 90];
+console.log(numbers.length, numbers, "'boshlanishdagi array'");
+
+let evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers.length, evenNumbers, "'juft sonlarga ajratilgan array'");
+
+
+
+
+
+``
+
+
+
+
+
+
+
+
